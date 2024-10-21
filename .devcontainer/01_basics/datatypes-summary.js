f8ea967d416cd1a13d15fb8b0f84539bbd1417a0
Anshu123-Goyal <139432748+Anshu123-Goyal@users.noreply.github.com>
2024-10-21 06:35:46 +0000

    datatype summary

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+// Primitive(Call by value:when copy data not original to changes)
+
+// 7 types: String,Number,Boolean,Null(Empty),Undefined,Symbol,BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id===anotherId);
+
+// const bigNumber = 70827701415427n
+
+// Reference (Non Primitive):Memory m reference directly allocate kiya ja skta hai...
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman","nagraj","doga"];//array
+let myObj= {   
+    name :"anshu",    // object
+    age :21,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp);
+console.log(typeof scoreValue);
+console.log(typeof myFunction);
